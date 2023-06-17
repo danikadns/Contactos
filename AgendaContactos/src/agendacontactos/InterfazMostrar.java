b3764d9a5d64466566c0b11e086a2cce5c749466
eliaspozuelos <eliaspozuelos256@gmail.com>
2023-06-17 09:35:10 -0600

    Interfaces no chafas modificadas por el moment xd modificadas

--- AgendaContactos/src/agendacontactos/InterfazMostrar.java
+++ AgendaContactos/src/agendacontactos/InterfazMostrar.java
@@ -209,6 +209,7 @@ public class InterfazMostrar extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void cerrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cerrarActionPerformed
